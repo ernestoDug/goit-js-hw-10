4698c6a044014e7aa88bacf2713f32a35aaca53f
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2023-06-27 23:48:47 +0300

    5v

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,9 +1,9 @@
-// функція  наповнення селекта
-import axios from 'axios'
+import axios, {isCancel, AxiosError} from 'axios'
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
 const MY_KEY = "axios.defaults.headers.common['x-api-key']=live_Bvmmb25LUgn0kerowwQa8P9jXzhxZ7PQweZaoNBCqAfNembGTRKEXQZy885vOj5o";
 const selectVar = document.querySelector(".breed-select");
 
+// функція  наповнення селекта
  function fetchBreeds() {
   
     axios
@@ -19,7 +19,11 @@ const selectVar = document.querySelector(".breed-select");
           return  selectVar.add(new Option(`${name}`,`${id}`))
                 })
               })
-             } 
+              .catch(error => {
+                alert(error)
+           
+              })
+                           } 
 
              export { BASE_URL, MY_KEY, selectVar, fetchBreeds}
               
--- src/index.js
+++ src/index.js
@@ -1,21 +1,60 @@
-import axios from 'axios'
+import axios, {isCancel, AxiosError} from 'axios'
+
 import { BASE_URL, MY_KEY, selectVar, fetchBreeds } from './cat-api.js'
 
 
+// https://api.thecatapi.com/v1/images/search?breed_ids=ідентифікатор_породи
 
+const URL_FOR_INFOCAT = "https://api.thecatapi.com/v1/images/search";
+
+const catHub = document.querySelector(".cat-info")
 
 fetchBreeds();
-// селект
 // вантажник
 const gg = document.querySelector(".loader");
 // слухач седекта
-selectVar.addEventListener('click', onChange);
-
+selectVar.addEventListener('change', onChange);
 
 
 function onChange (event)
 {
+    let id = event.target.value;
+    console.log(id)
     console.log(event.target)
+
+    fetchCatByBreed(id);
 }
 
+
+        function fetchCatByBreed(id){
+        axios
+        .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}`, {
+        
+        Headers: {
+            'x-api-key': `${MY_KEY}`
+          }
+        })
+        .then(response => {
+            response.data.map(({url}) =>{
+        // додаємо до селекту опції
+        console.log(response.data)
+              catHub.innerHTML = `<img src="${url}" width: 50% alt="">`
+              
+                    })
+                  })
+                  .catch(error => {
+                    alert(error)
+               
+                  })
+        }
+    //     response.data.map(({id, name}) =>{
+    // // додаємо до селекту опції
+    //       return  selectVar.add(new Option(`${name}`,`${id}`))
+    //             })
+    //           })
+    //           .catch(error => {
+    //             alert(error)
+           
+    //           })
+                        
 

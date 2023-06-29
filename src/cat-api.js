2561724843bf492dabfbf865a3d2a1124752557f
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2023-06-29 20:35:09 +0300

    7v

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,7 @@
 import axios, {isCancel, AxiosError} from 'axios'
-import { io } from './index.js'
+import { selecter } from './index.js'
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
-const MY_KEY = "axios.defaults.headers.common['x-api-key']=live_Bvmmb25LUgn0kerowwQa8P9jXzhxZ7PQweZaoNBCqAfNembGTRKEXQZy885vOj5o";
+const MY_KEY = "live_Bvmmb25LUgn0kerowwQa8P9jXzhxZ7PQweZaoNBCqAfNembGTRKEXQZy885vOj5o";
 const selectVar = document.querySelector(".breed-select");
 
 // функція  наповнення селекта
@@ -17,13 +17,13 @@ const selectVar = document.querySelector(".breed-select");
     .then(response => {
         // response.data.map(({id, name, description, temperament}) =>{
     // додаємо до селекту опції
-    console.log("even,,", response.data)
+    console.log("из кет в ио", response.data)
           // return  selectVar.add(new Option(`${name}`, `${id}`, `${description}`, `${temperament}`))
           //       })
-          return response.data
+          return response.data;
               })
             // })
-            .then(io)
+            .then(selecter)
               .catch(error => {
                 alert(error)
            
--- src/index.js
+++ src/index.js
@@ -8,62 +8,56 @@ import { BASE_URL, MY_KEY, selectVar, fetchBreeds } from './cat-api.js'
 const URL_FOR_INFOCAT = "https://api.thecatapi.com/v1/images/search";
 
 const catHub = document.querySelector(".cat-info");
-const miu = document.querySelector(".miu")
+// const miuD = document.querySelector(".miuDescr");
+// const miuT = document.querySelector(".miuTemper")
+// let dataS = null;
 
 fetchBreeds()
 
 
-export function io (data) {
-  console.log("4545", data)
-  data.map(({id, name, description, temperament}) =>{
+export function selecter (data) {
+  // console.log("4545", data)
+  data.map(({id, name, temperament, descriptionz}) =>{
 // // додаємо до селекту опції
-// console.log("lknn", description)
+// console.log("lknn", data)
     selectVar.add(new Option(`${name}`, `${id}`))
-    console.log("tem", description)
-    console.log("tem2", temperament)
-
-  return description, temperament
-        })
-      }
-                     
+        // let mass = {id: `${id}`, name: `${name}`, description: `${description}`, temperament: `${temperament}`}
+        // console.log("tem", `${description}`)
+        return data
+      })
+     console.log(data, "io")
+}         
 
 // вантажник
 const gg = document.querySelector(".loader");
-// слухач седекта
+// слухач сеLекта
 selectVar.addEventListener('change', onChange);
 
 
 function onChange (event)
 {
-    let id = event.target.value;
-    let des = event.target.title
-    let sm = event.target.temperament
+    const id = event.target.value;
+    // let des = event.target
+// console.log(responseText, "4654654pppppppppppppp")
+const vib = selectVar.selectedIndex;
+const opt = selectVar.options
 
-    console.log(id, "787987");
-    console.log(event.target, "event");
-    // console.log(des, "87978;;;")
-    console.log(sm)
-
+    // console.log(opt[vib], "555555555555555555");****************************************
+    console.log(selectVar.value, "na change");
 
     fetchCatByBreed(id);
-
-
   }
         function fetchCatByBreed(id){
         axios
-        .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}`, {
-        
-        Headers: {
-            'x-api-key': `${MY_KEY}`
-          }
-        })
+        .get(`https://api.thecatapi.com/v1/images/search?api_key=${MY_KEY}&breeds_ids=${id}&has_breeds=1`)
+                    
         .then(response => {
-            response.data.map(({url, id}) =>{
+            response.data.map(({url}) =>{
         // додаємо до селекту опції
-        console.log(response.data)
-        catHub.innerHTML = `<img src="${url}" width: 50% alt="">`
-        // miu.textContent = event.target.description;
-
+        console.log(response.data, " по айди")
+        catHub.innerHTML = `<img src="${url}"  alt="">`
+// miuD.textContent = "data.description";
+// console.log(data.description)
         
                     })
                   })

bf307d98d1963973ca7031d1d2624da38d61d667
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2023-07-01 01:16:41 +0300

    12

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,35 +1,31 @@
-import axios, {isCancel, AxiosError} from 'axios'
-import { selecter } from './index.js'
+import axios, { isCancel, AxiosError } from 'axios';
+
+import { selecter, loaderVar } from './index.js';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
-const MY_KEY = "live_Bvmmb25LUgn0kerowwQa8P9jXzhxZ7PQweZaoNBCqAfNembGTRKEXQZy885vOj5o";
-const selectVar = document.querySelector(".breed-select");
+const MY_KEY =
+  'live_Bvmmb25LUgn0kerowwQa8P9jXzhxZ7PQweZaoNBCqAfNembGTRKEXQZy885vOj5o';
+const selectVar = document.querySelector('.breed-select');
 
 // функція  наповнення селекта
- function fetchBreeds() {
-  
-    axios
+function fetchBreeds() {
+  axios
     .get(`${BASE_URL}?api-key=${MY_KEY}`, {
-    
       // Headers: {
       //   'x-api-key': `${MY_KEY}`
       // }
     })
     .then(response => {
-        // response.data.map(({id, name, description, temperament}) =>{
-    // додаємо до селекту опції
-    console.log("из кет в ио", response.data)
-          // return  selectVar.add(new Option(`${name}`, `${id}`, `${description}`, `${temperament}`))
-          //       })
-          return response.data;
-              })
-            // })
-            .then(selecter)
-              .catch(error => {
-                alert(error)
-           
-              })
-                           } 
+      // console.log("из кет в selecter", response.data)
 
-             export { BASE_URL, MY_KEY, selectVar, fetchBreeds}
-              
+      return response.data;
+    })
+    .then(selecter)
+    .catch(error => {
+      Notify.warning(`❌ Oops! Something went wrong! Try reloading the page!`);
+    });
+}
+
+export { BASE_URL, MY_KEY, selectVar, fetchBreeds };
--- src/index.js
+++ src/index.js
@@ -1,83 +1,116 @@
-import axios, {isCancel, AxiosError} from 'axios'
+import axios, { isCancel, AxiosError } from 'axios';
 
-import { BASE_URL, MY_KEY, selectVar, fetchBreeds } from './cat-api.js'
+import { BASE_URL, MY_KEY, selectVar, fetchBreeds } from './cat-api.js';
 
+import SlimSelect from 'slim-select';
 
-import SlimSelect from 'slim-select'
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 // https://api.thecatapi.com/v1/images/search?breed_ids=ідентифікатор_породи
 
-const URL_FOR_INFOCAT = "https://api.thecatapi.com/v1/images/search";
+const URL_FOR_INFOCAT = 'https://api.thecatapi.com/v1/images/search';
 
-const catHub = document.querySelector(".cat-info");
-const miuD = document.querySelector(".miuDescr");
+// для фото кота
+const catHub = document.querySelector('.cat-info');
+// для опису кота
+const miuD = document.querySelector('.miuDescr');
+// для індикатору завантаження
+const loaderVar = document.querySelector('.loaderWrap');
 
+// // селект не бачу
+// selectVar.classList.add("breed-select--hidden");
+// // індикатор бачу
+// loaderVar.classList.add("loaderWrap");
 
+// виклик функції отримання пород
+fetchBreeds();
 
-fetchBreeds()
+function selecter(data) {
+  data.map(({ id, name }) => {
+    //    if(data)
+    // {
+    //   // ящко відповідь поичнаю бачити селект
+    //   // видаляю хіден селекта
+    //   selectVar.classList.remove("breed-select--hidden")
+    //   // додаю бачимость селекту
+    //   selectVar.classList.add("breed-select")
+    //   // / ящко відповідь
+    //   // прибираю бачимість індикатора
+    //   loaderVar.classList.remove("loaderWrap");
+    //   // додаю хіден індикатор
+    //   loaderVar.classList.add("loaderWrap--hidden");
+    // }
 
-
-export function selecter (data) {
-  data.map(({id, name}) =>{
     // // додаємо до селекту опції
-    selectVar.add(new Option (`${name}`, `${id}`))
-    // console.log("tem", `${description}`)
+    selectVar.add(new Option(`${name}`, `${id}`));
     // return data
-  })
+    //  loaderVar.classList.remove("loaderWrap");
+    //  loaderVar.classList.add("loaderWrap--hidden");
+    //  selectVar.classList.add("breed-select")
+    //  selectVar.classList.remove("breed-select--hidden")
+  });
+  // створення екзепляру селекту бібліотеки
   new SlimSelect({
     select: selectVar,
     settings: {
-      placeholderText: 'розпочати котопошук',
       allowDeselect: true,
       showSearch: true,
-      searchText:  'котяра не знайден',
+      searchText: 'котяра не знайден',
       searchPlaceholder: 'розпочати котопошук',
-      searchHighlight: true    }
-        })
-    //  console.log(data, "io")
-}         
+      searchHighlight: true,
+    },
+  });
+
+  //  console.log(data, "io")
+}
 
 // вантажник
-const gg = document.querySelector(".loader");
+const gg = document.querySelector('.loader');
 // слухач сеLекта
 selectVar.addEventListener('change', onChange);
 
+// обробник селекта
+function onChange(event) {
+  const idBreed = event.target.value;
+  // let des = event.target
+  const vib = selectVar.selectedIndex;
+  const opt = selectVar.options;
 
-function onChange (event)
-{
-    const idBreed = event.target.value;
-    // let des = event.target
-    const vib = selectVar.selectedIndex;
-    const opt = selectVar.options
+  // console.log(opt[vib], "555555555555555555");****************************************
+  console.log(selectVar.value, idBreed, 'na change');
 
+  fetchCatByBreed(idBreed);
+}
+function fetchCatByBreed(idBreed) {
+  axios
+    .get(
+      `${URL_FOR_INFOCAT}?api_key=${MY_KEY}&has_breeds=1&breeds_ids=${idBreed}&sub_id`
+    )
 
-    // console.log(opt[vib], "555555555555555555");****************************************
-    console.log(selectVar.value, idBreed,  "na change");
-
-    fetchCatByBreed(idBreed);
-  }
-        function fetchCatByBreed(idBreed){
-        axios
-        .get(`${URL_FOR_INFOCAT}?api_key=${MY_KEY}&has_breeds=1&breeds_ids=${idBreed}&sub_id`)
-                    
-        .then(response => {
-            response.data.map(({url, breeds: [{name, description, temperament}]}) =>{
-        // додаємо до селекту опції
-        console.log( response.data, description,"ОПИС", temperament, "ТemМП", name)
-        catHub.innerHTML = 
-        `<div class = "wrapp">
+    .then(response => {
+      response.data.map(
+        ({ url, breeds: [{ name, description, temperament }] }) => {
+          // додаємо до селекту опції
+          console.log(
+            response.data,
+            description,
+            'ОПИС',
+            temperament,
+            'ТemМП',
+            name
+          );
+          catHub.innerHTML = `<div class = "wrapp">
         <h1 class="catName"> ${name}</h1>
        <h2 class="catDescr"> ${description}</h2>
        <h3 class="catTemp">${temperament}</h3>
        </div>
-       <div class="photoCatWrap"> <img class="catPortret" src="${url}" alt="${name}"</div>`
-                    })
-                  })
-                  .catch(error => {
-                    alert(error)
-               
-                  })
+       <div class="photoCatWrap"> <img class="catPortret" src="${url}" alt="${name}"</div>`;
         }
+      );
+    })
+    .catch(error => {
+      Notify.warning(`❌ Oops! Something went wrong! Try reloading the page!`);
+    });
+}
 
-
+export { loaderVar, selecter };
